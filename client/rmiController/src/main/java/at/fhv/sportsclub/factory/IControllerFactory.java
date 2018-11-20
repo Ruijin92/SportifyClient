45889c2bed3dd8ec37202d173f633a62e78de856
Lukas <35338529+LukiStadelmann@users.noreply.github.com>
2018-11-20 16:58:48 +0100

    Added and changed controller and factory

--- client/rmiController/src/main/java/at/fhv/sportsclub/factory/IControllerFactory.java
+++ client/rmiController/src/main/java/at/fhv/sportsclub/factory/IControllerFactory.java
@@ -1,17 +1,18 @@
 package at.fhv.sportsclub.factory;
 
-import at.fhv.sportsclub.controller.interfaces.IDepartmentController;
-import at.fhv.sportsclub.controller.interfaces.IPersonController;
+import at.fhv.sportsclub.controller.interfaces.*;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.List;
 
-/**
+/*
  * Created by Alex on 06.11.2018.
  */
 
 public interface IControllerFactory extends Remote {
     IPersonController getPersonController() throws RemoteException;
     IDepartmentController getDepartmentController() throws RemoteException;
+    ITeamController getTeamController() throws RemoteException;
+    ITournamentController getTournamentController() throws RemoteException;
+    IAuthenticationController getAuthenticationController() throws RemoteException;
 }
