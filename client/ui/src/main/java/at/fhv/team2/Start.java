516ed1676ad6c04d1663e1cedeeac95e36a11e69
Lukas <35338529+LukiStadelmann@users.noreply.github.com>
2018-11-20 16:59:54 +0100

    Changed the RunMe and Start method for the tests

--- client/ui/src/main/java/at/fhv/team2/Start.java
+++ client/ui/src/main/java/at/fhv/team2/Start.java
@@ -1,11 +1,15 @@
 package at.fhv.team2;
 
+import at.fhv.sportsclub.controller.interfaces.IAuthenticationController;
+import at.fhv.sportsclub.factory.IControllerFactory;
+import at.fhv.sportsclub.model.security.SessionDTO;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
@@ -22,6 +26,17 @@ public class Start extends Application {
 
         Registry registry = LocateRegistry.getRegistry(1099);
         DataProvider.setRegistry(registry);
+
+       /* IControllerFactory controllerFactory = (IControllerFactory) registry.lookup("ControllerFactory");
+        IAuthenticationController authenticationController = controllerFactory.getAuthenticationController();
+
+        SessionDTO session = authenticationController.authenticate(pw, pw.toCharArray());
+        DataProvider.setSession(session);
+*/
+        String pw = "snoop@do.gg";
+
+        DataProvider dataProvider = DataProvider.get();
+        dataProvider.authenticate("snoop@do.gg", pw.toCharArray());
 
         Parent root = FXMLLoader.load(getClass().getResource("/MainPage.fxml"));
         primaryStage.setTitle("Sportverwaltung");
