08c2235074e7280dfd66d3368efa55978890fd2e
Lukas <35338529+LukiStadelmann@users.noreply.github.com>
2018-12-04 00:41:51 +0100

    Added the funcionality

--- client/ui/src/main/java/at/fhv/team2/wettkampf/Encounter.java
+++ client/ui/src/main/java/at/fhv/team2/wettkampf/Encounter.java
@@ -1,21 +1,35 @@
 package at.fhv.team2.wettkampf;
 
+import at.fhv.sportsclub.controller.interfaces.ITournamentController;
+import at.fhv.sportsclub.model.common.ModificationType;
+import at.fhv.sportsclub.model.tournament.EncounterDTO;
+import at.fhv.sportsclub.model.tournament.ParticipantDTO;
 import at.fhv.sportsclub.model.tournament.TournamentDTO;
+import at.fhv.team2.wettkampf.ViewModels.EncounterViewModel;
+import at.fhv.team2.wettkampf.ViewModels.ParticipantViewModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.util.StringConverter;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Observable;
 import java.util.ResourceBundle;
 
 public class Encounter extends HBox implements Initializable {
 
     private TournamentDTO tournamentDTO;
+    private ArrayList<EncounterViewModel> encounters;
+    private ObservableList<EncounterViewModel> tableEncounters;
 
     public TableView table;
     public TableColumn homeColumn;
@@ -30,6 +44,7 @@ public class Encounter extends HBox implements Initializable {
     public TextField guestResult;
     public Button resultButton;
     public Button saveButton;
+
 
     public Encounter(TournamentDTO tournamentDTO) {
         this.tournamentDTO = tournamentDTO;
@@ -47,15 +62,28 @@ public class Encounter extends HBox implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        addTeamsToComboBox();
+        addToTable();
+        tableEncounters = FXCollections.observableArrayList();
+        table.setItems(tableEncounters);
     }
 
     public void addEncounter(ActionEvent event) {
+        Object homeTeam = homeCombo.getSelectionModel().getSelectedItem();
+        Object guestTeam = guestCombo.getSelectionModel().getSelectedItem();
 
+        //TODO:Date wird aus TournamentDTO gezogen + Time wird in diesem Screen gesetzt
+        encounters = new ArrayList<>();
+        encounters.add(new EncounterViewModel(null, null, null, (ParticipantViewModel) homeTeam, (ParticipantViewModel) guestTeam, 0, 0));
+        tableEncounters.addAll(FXCollections.observableArrayList(encounters));
     }
 
     public void addResult(ActionEvent event) {
-
+        EncounterViewModel encounter = (EncounterViewModel) table.getSelectionModel().getSelectedItem();
+        encounter.setHomePoints(Integer.parseInt(homeResult.getText()));
+        encounter.setGuestPoints(Integer.parseInt(guestResult.getText()));
+        homeResult.clear();
+        guestResult.clear();
     }
 
     public void clickItem(MouseEvent event) {
@@ -63,6 +91,50 @@ public class Encounter extends HBox implements Initializable {
     }
 
     public void saveEncounter(ActionEvent event) {
+        ArrayList<EncounterDTO> encounterDTOS = new ArrayList<>();
+        for (EncounterViewModel encounter : encounters) {
+            encounterDTOS.add(new EncounterDTO(null, null, 0, encounter.getHomeTeamModel().getId(), encounter.getGuestTeamModel().getId(), encounter.getHomePoints(), encounter.getGuestPoints(), null, ModificationType.MODIFIED));
+        }
+    }
+
+    private void addTeamsToComboBox() {
+        ArrayList<ParticipantViewModel> teams = new ArrayList<>();
+        for (ParticipantDTO team : tournamentDTO.getTeams()) {
+            teams.add(new ParticipantViewModel(team.getId(), team.getTeam(), team.getTeamName(), null));
+        }
+        ObservableList<ParticipantViewModel> allItems = FXCollections.observableArrayList(teams);
+        homeCombo.setItems(allItems);
+        homeCombo.setConverter(new StringConverter<ParticipantViewModel>() {
+            @Override
+            public String toString(ParticipantViewModel object) {
+                return object.getTeamName();
+            }
+
+            @Override
+            public ParticipantViewModel fromString(String string) {
+                return null;
+            }
+        });
+        guestCombo.setItems(allItems);
+        guestCombo.setConverter(new StringConverter<ParticipantViewModel>() {
+            @Override
+            public String toString(ParticipantViewModel object) {
+                return object.getTeamName();
+            }
+
+            @Override
+            public ParticipantViewModel fromString(String string) {
+                return null;
+            }
+        });
+    }
+
+    private void addToTable() {
+        homeColumn.setCellValueFactory(new PropertyValueFactory<>("homeTeam"));
+        guestColumn.setCellValueFactory(new PropertyValueFactory<>("guestTeam"));
+        homeResultColumn.setCellValueFactory(new PropertyValueFactory<>("homePoints"));
+        guestResultColumn.setCellValueFactory(new PropertyValueFactory<>("guestPoints"));
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
 
     }
 }
