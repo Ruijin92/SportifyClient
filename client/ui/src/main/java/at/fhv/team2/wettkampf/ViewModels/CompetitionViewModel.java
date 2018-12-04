a3538d746d547c5ce822f428d50610514d173886
Lukas <35338529+LukiStadelmann@users.noreply.github.com>
2018-12-04 19:50:06 +0100

    Changed the CompetitionViewModel

--- client/ui/src/main/java/at/fhv/team2/wettkampf/ViewModels/CompetitionViewModel.java
+++ client/ui/src/main/java/at/fhv/team2/wettkampf/ViewModels/CompetitionViewModel.java
@@ -11,6 +11,7 @@ public class CompetitionViewModel {
     private SimpleStringProperty leagueName;
     private SimpleStringProperty sportsName;
     private SimpleStringProperty league;
+    private SimpleStringProperty date;
 
     private List<EncounterViewModel> encounters;
     private List<ParticipantViewModel> participants;
@@ -102,5 +103,18 @@ public class CompetitionViewModel {
     public void setParticipants(List<ParticipantViewModel> participants) {
         this.participants = participants;
     }
+
+    public String getDate() {
+        return date.get();
+    }
+
+    public SimpleStringProperty dateProperty() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date.set(date);
+    }
+
     //endregion
 }
