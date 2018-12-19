75ed5fe6079f1c163a00076779df141235652292
Lukas <35338529+LukiStadelmann@users.noreply.github.com>
2018-12-19 11:37:17 +0100

    Updated the Start

--- client/ui/src/main/java/at/fhv/team2/Start.java
+++ client/ui/src/main/java/at/fhv/team2/Start.java
@@ -6,8 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Properties;
 
 /**
  * Created by Uray Örnek on 11/5/2018.
@@ -19,7 +22,11 @@ public class Start extends Application {
     }
 
     public void start(Stage primaryStage) throws Exception {
+        Properties env = new Properties();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+        InitialContext ctx = new InitialContext(env);
 
+        DataProviderFactory.setInitialContext(ctx);
 
         Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
         primaryStage.setTitle("Sportverwaltung");
